a7813a1a8954c8eebfcec35e21842b0adefabfa5
Pavlo <paul.antonenko.w@gmail.com>
2025-02-06 19:00:48 +0200

    Pricing, Accordion, Input
    Added pricing section, accordion section, and section with input uses useState react

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,7 +1,21 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleQuestion } from '@fortawesome/free-regular-svg-icons';
+
 import './app.scss';
 import '../../variables.scss';
 
 function App() {
+  const [isFocused, setIsFocused] = useState(false);
+  const [hasError, setHasError] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  const handleBlur = (event) => {
+    setIsFocused(false);
+    setIsFilled(event.target.value.trim() !== '');
+    setHasError(event.target.value.trim() === '');
+  };
+
   return (
     <div className='application'>
       <header className="header">
@@ -193,6 +207,305 @@ function App() {
                     </div>
                   </div>
                 </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="pricing">
+          <div className="container">
+            <h3 className="pricing__title">Pricing Tiers</h3>
+            <div className="pricing__header">
+              <h2 className="pricing__header-title">Fit for all your needs</h2>
+              <p className="pricing__header-subtitle">Pick the plan that suits you today and step up as your demands grow - our flexible options have your journey mapped out.</p>
+            </div>
+            <div className="pricing__content">
+              <div className="pricing__toggle">
+                <button className="pricing__toggle-btn active">Monthly</button>
+                <button className="pricing__toggle-btn">Annually</button>
+              </div>
+              <div className="pricing__cards">
+                <div className="pricing__card">
+                  <div className="pricing__header-hover">
+                    <p className="pricing__header-hover-text">Most Popular</p>
+                  </div>
+                  <div className="pricing__card-wrapper">
+                    <div className="pricing__card-header">
+                      <h4 className="pricing__card-title">Basic Plan</h4>
+                      <p className="pricing__card-subtitle">Access to a curated selection of abstract images</p>
+                      <div className="pricing__card-price">
+                        <span className="pricing__card-price-value">$9.99</span>
+                        <span className="pricing__card-price-period">/month</span>
+                      </div>
+                      <p className="pricing__billed">Billed monthly</p>
+                    </div>
+                    <div className="pricing__card-content">
+                      <ul className="pricing__card-list">
+                        <li className="pricing__card-item">
+                          <span className="pricing__card-icon">
+                            <img src="/images/icons/check.svg" alt="Icon Checked" />
+                            <p className="pricing__card-text">Standard quality images</p>
+                          </span>
+                        </li>
+                        <li className="pricing__card-item">
+                          <span className="pricing__card-icon">
+                            <img src="/images/icons/check.svg" alt="Icon Checked" />
+                            <p className="pricing__card-text">Limited to personal use</p>
+                          </span>
+                        </li>
+                        <li className="pricing__card-item">
+                          <span className="pricing__card-icon">
+                            <img src="/images/icons/check.svg" alt="Icon Checked" />
+                            <p className="pricing__card-text">Email support</p>
+                          </span>
+                        </li>
+                      </ul>
+                      <button className="pricing__card-btn secondary-btn">Buy now</button>
+                    </div>
+                  </div>
+                </div>
+                <div className="pricing__card">
+                  <div className="pricing__header-hover">
+                    <p className="pricing__header-hover-text">Most Popular</p>
+                  </div>
+                  <div className="pricing__card-wrapper">
+                    <div className="pricing__card-header">
+                      <h4 className="pricing__card-title">Standard Plan</h4>
+                      <p className="pricing__card-subtitle">Next-level Integrations, priced economically</p>
+                      <div className="pricing__card-price">
+                        <span className="pricing__card-price-value">$19.99</span>
+                        <span className="pricing__card-price-period">/month</span>
+                      </div>
+                      <p className="pricing__billed">Billed monthly</p>
+                    </div>
+                    <div className="pricing__card-content">
+                      <ul className="pricing__card-list">
+                        <li className="pricing__card-item">
+                          <span className="pricing__card-icon">
+                            <img src="/images/icons/check.svg" alt="Icon Checked" />
+                            <p className="pricing__card-text">Expanded library with more diverse abstract images</p>
+                          </span>
+                        </li>
+                        <li className="pricing__card-item">
+                          <span className="pricing__card-icon">
+                            <img src="/images/icons/check.svg" alt="Icon Checked" />
+                            <p className="pricing__card-text">High-resolution images available</p>
+                          </span>
+                        </li>
+                        <li className="pricing__card-item">
+                          <span className="pricing__card-icon">
+                            <img src="/images/icons/check.svg" alt="Icon Checked" />
+                            <p className="pricing__card-text">Suitable for commercial use</p>
+                          </span>
+                        </li>
+                        <li className="pricing__card-item">
+                          <span className="pricing__card-icon">
+                            <img src="/images/icons/check.svg" alt="Icon Checked" />
+                            <p className="pricing__card-text">Priority email support</p>
+                          </span>
+                        </li>
+                        <li className="pricing__card-item">
+                          <span className="pricing__card-icon">
+                            <img src="/images/icons/check.svg" alt="Icon Checked" />
+                            <p className="pricing__card-text">Advanced analytics</p>
+                          </span>
+                        </li>
+                      </ul>
+                      <button className="pricing__card-btn secondary-btn">Buy now</button>
+                    </div>
+                  </div>
+                </div>
+                <div className="pricing__card">
+                  <div className="pricing__header-hover">
+                    <p className="pricing__header-hover-text">Most Popular</p>
+                  </div>
+                  <div className="pricing__card-wrapper">
+                    <div className="pricing__card-header">
+                      <h4 className="pricing__card-title">Basic Plan</h4>
+                      <p className="pricing__card-subtitle">Access to a curated selection of abstract images</p>
+                      <div className="pricing__card-price">
+                        <span className="pricing__card-price-value">$29.99</span>
+                        <span className="pricing__card-price-period">/month</span>
+                      </div>
+                      <p className="pricing__billed">Billed monthly</p>
+                    </div>
+                    <div className="pricing__card-content">
+                      <ul className="pricing__card-list">
+                        <li className="pricing__card-item">
+                          <span className="pricing__card-icon">
+                            <img src="/images/icons/check.svg" alt="Icon Checked" />
+                            <p className="pricing__card-text">Full access to the entire image library, including exclusive content</p>
+                          </span>
+                        </li>
+                        <li className="pricing__card-item">
+                          <span className="pricing__card-icon">
+                            <img src="/images/icons/check.svg" alt="Icon Checked" />
+                            <p className="pricing__card-text">Highest quality images, including premium collections</p>
+                          </span>
+                        </li>
+                        <li className="pricing__card-item">
+                          <span className="pricing__card-icon">
+                            <img src="/images/icons/check.svg" alt="Icon Checked" />
+                            <p className="pricing__card-text">Commercial and resale rights</p>
+                          </span>
+                        </li>
+                        <li className="pricing__card-item">
+                          <span className="pricing__card-icon">
+                            <img src="/images/icons/check.svg" alt="Icon Checked" />
+                            <p className="pricing__card-text">Dedicated customer support line</p>
+                          </span>
+                        </li>
+                        <li className="pricing__card-item">
+                          <span className="pricing__card-icon">
+                            <img src="/images/icons/check.svg" alt="Icon Checked" />
+                            <p className="pricing__card-text">24/7 support response time</p>
+                          </span>
+                        </li>
+                        <li className="pricing__card-item">
+                          <span className="pricing__card-icon">
+                            <img src="/images/icons/check.svg" alt="Icon Checked" />
+                            <p className="pricing__card-text">Advanced analytics and insights</p>
+                          </span>
+                        </li>
+                      </ul>
+                      <button className="pricing__card-btn secondary-btn">Buy now</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="faq">
+          <div className="container">
+            <div className="faq__header">
+              <h2 className="faq__header-title">Fit for all your needs</h2>
+              <p className="faq__header-subtitle">Pick the plan that suits you today and step up as your demands grow - our flexible options have your journey mapped out.</p>
+            </div>
+            <div className="faq__wrapper">
+              <div className="faq__something"></div>
+              <div className="faq__accordion">
+                <details className="faq__accordion-item">
+                  <summary className="faq__accordion-summary">
+                    <span className="faq__accordion-title">What types of images are available on your platform?</span>
+                  </summary>
+                  <div className="faq__accordion-content">
+                    <p>
+                      Our platform offers a diverse range of abstract images to suit various preferences and needs. From vibrant geometric patterns to soothing landscapes, we strive to provide a wide selection to cater to different tastes.
+                    </p>
+                  </div>
+                </details>
+                <details className="faq__accordion-item">
+                  <summary className="faq__accordion-summary">
+                    <span className="faq__accordion-title">How can I access and download images from your platform?</span>
+                  </summary>
+                  <div className="faq__accordion-content">
+                    <p>
+                      Accessing and downloading images from our platform is simple. Upon signing up and logging in, users can browse through our curated collection and download their chosen images directly to their devices with just a few clicks.
+                    </p>
+                  </div>
+                </details>
+                <details className="faq__accordion-item">
+                  <summary className="faq__accordion-summary">
+                    <span className="faq__accordion-title">Do you offer free images, or is there a subscription required?</span>
+                  </summary>
+                  <div className="faq__accordion-content">
+                    <p>
+                      We provide both free and premium images on our platform. Users can explore a selection of free images without any subscription. For access to our entire library and additional features, we offer subscription plans tailored to different user needs.
+                    </p>
+                  </div>
+                </details>
+                <details className="faq__accordion-item">
+                  <summary className="faq__accordion-summary">
+                    <span className="faq__accordion-title">What payment methods do you accept for subscriptions?</span>
+                  </summary>
+                  <div className="faq__accordion-content">
+                    <p>
+                      We accept a variety of payment methods, including credit/debit cards and online payment gateways, to make the subscription process convenient for our users.
+                    </p>
+                  </div>
+                </details>
+                <details className="faq__accordion-item">
+                  <summary className="faq__accordion-summary">
+                    <span className="faq__accordion-title">Can I cancel or modify my subscription at any time?</span>
+                  </summary>
+                  <div className="faq__accordion-content">
+                    <p>
+                      Yes, absolutely. You have the flexibility to cancel or modify your subscription at any time through your account settings. Changes will take effect immediately, ensuring you have full control over your subscription preferences.
+                    </p>
+                  </div>
+                </details>
+                <details className="faq__accordion-item">
+                  <summary className="faq__accordion-summary">
+                    <span className="faq__accordion-title">How frequently do you update your image collection?</span>
+                  </summary>
+                  <div className="faq__accordion-content">
+                    <p>
+                      We regularly update our image collection with fresh and captivating content to keep our users inspired and engaged. New images are added consistently to ensure there's always something new to discover on our platform.
+                    </p>
+                  </div>
+                </details>
+              </div>
+              <div className="faq__support-wrapper">
+                <div className="faq__support-info">
+                  <h4 className="faq__support-title">Can’t find the answer you’re looking for?</h4>
+                  <p className="faq__support-description">
+                    Reach out to our&nbsp;
+                    <a className="aq__support-description-link" href="#">customer support</a>
+                    &nbsp;team.
+                  </p>
+                </div>
+                <button className="faq__support-btn primary-btn">Get in touch</button>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="answers">
+          <div className="container">
+            <div className="answers__content">
+              <div className="answers__info">
+                <h2 className="answers__title">Get the finest curated abstracts delivered weekly to your inbox</h2>
+                <ul className="answers__list">
+                  <li className="answers__item">
+                    <span className="answers__icon">
+                      <img src="/images/icons/check.svg" alt="Icon Checked" />
+                      <p className="answers__text">Exclusive access to new abstract images and collections</p>
+                    </span>
+                  </li>
+                  <li className="answers__item">
+                    <span className="answers__icon">
+                      <img src="/images/icons/check.svg" alt="Icon Checked" />
+                      <p className="answers__text">Unlock special promotions only for subscribers</p>
+                    </span>
+                  </li>
+                  <li className="answers__item">
+                    <span className="answers__icon">
+                      <img src="/images/icons/check.svg" alt="Icon Checked" />
+                      <p className="answers__text">Regular doses of artistic inspiration</p>
+                    </span>
+                  </li>
+                </ul>
+                <div className="answers__forms">
+                  <form className="answers__form" action="email">
+                    <div className="answers__form-input-wrap">
+                      <input
+                        className={`input ${hasError ? "error" : isFocused ? "focused" : isFilled ? "filled" : ""}`}
+                        type="email"
+                        onFocus={() => setIsFocused(true)}
+                        onBlur={handleBlur}
+                        name="email"
+                        placeholder="name@email.com"
+                      />
+                      <FontAwesomeIcon icon={faCircleQuestion} className={`input-icon ${hasError ? "icon-error" : "icon-normal"}`} />
+                    </div>
+                    {isFocused && !hasError && <span className="hint-text">This is a hint text.</span>}
+                    {hasError && <span className="error-message">This is an error message.</span>}
+                    <button className="answers__btn primary-btn" type="submit">Subscribe</button>
+                  </form>
+                  <p className="answers__form-safely-text">We only send you the best! No spam.</p>
+                </div>
+              </div>
+              <div className="answers__img">
+                <img src="/images/answer.png" alt="Answer images" />
               </div>
             </div>
           </div>
